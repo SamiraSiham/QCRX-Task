4f90e7eb95f318418a42e38dd86ee8911e56ca6c
Samira Siham <88947861+SamiraSiham@users.noreply.github.com>
2024-07-20 16:37:02 +0100

    hosting

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from './LandingComponents/Navbar'
 
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <div>
+    <Navbar/>
+    </div>
   )
 }
 
---
+++ src/LandingComponents/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import logoWeb from '../images/full logo.png';
+import logoMobile from '../images/mobile logo.png';
+import { isMobile } from 'react-device-detect';
+const Navbar = () => {
+  const imageSrc = isMobile ? logoMobile : logoWeb;
+  const imageStyle = isMobile;
+  return (
+    <div className='shadow-md w-full fixed top-0 left-0'>
+        <div className='md:flex bg-white py-4'>
+          <img src={imageSrc} alt="Website logo" />
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
